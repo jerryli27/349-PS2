5a14bdf3f0171328c03941762d3acc3b17da4bc8
IvyZX <ivyzhengx@gmail.com>
2015-04-28 01:11:37 -0500

    add the main function of building a decision tree
    and improved some helper functions

--- decisionTree.py
+++ decisionTree.py
@@ -1,30 +1,31 @@
-__author__ = 'Jerry'
+__author__ = ['Jerry', 'Ivy']
 import math
+import csv
 
-data = []
+thedata = []
+ATTR_SIZE = 14
 
 # region read data
 # The following function reads a training file in csv and write all its data in the data array.
 def readTrainFile():
     gazeFile = open('btrain.csv', 'r')
-    lineCounter = 0
-    # The data is recorded in column by column format, because I think this is a better way to store data when creating a decision tree.
+    gazeFile = csv.reader(gazeFile, delimiter=",")
+    data = [[] for i in range(ATTR_SIZE)]
+    # The data is recorded in column by column format, because I think this is a better way to store data
+    # when creating a decision tree
     for line in gazeFile:
-        line = line.split(",")
-        if lineCounter == 0:
-            for i in range(0, line.__len__()):
-                data.append([line[i]])
+        if (line[-1] == '?'):
+            pass
         else:
-            for i in range(0, line.__len__()):
+            for i in range(ATTR_SIZE):
                 data[i].append(try_to_float(line[i]))
-        lineCounter += 1
-    pass
+    return data
 
 
 # endregion
 
 # region decision tree building
-def findBestSplit():
+def findBestSplit(data):
     # Initialize
     best_attr = -1  # The attribute that a split will produce the most decrease in entropy
     best_split = None  # The value for splitting
@@ -39,7 +40,7 @@ def findBestSplit():
         sumNeg = 0
         counterNeg = 0
         for j in range(0, data[i].__len__()):
-            if is_number(data[i][j]):
+            if isinstance(data[i][j], float):
                 if data[data.__len__() - 1][j] == 1:
                     sumPos += data[i][j]
                     counterPos += 1
@@ -61,11 +62,12 @@ def findBestSplit():
                 min_ent = ent
                 best_attr = i
     print "The best split is in attribute " + str(best_attr) +":"+data[best_attr][0] \
-          + " with split=" + str(best_split) + " and entropy=" + str(
-        min_ent)
+          + " with split=" + str(best_split) + " and entropy=" + str(min_ent)
+    return best_attr, best_split, min_ent
 
 # This is the helper function that tries to find the best split for one attribute.
-# Input: initSplit (the split starting point), attrList (list of attributes), resultList (List of the result, which is either 0 or 1)
+# Input: initSplit (the split starting point), attrList (list of attributes),
+# resultList (List of the result, which is either 0 or 1)
 # Return: minEnt (The smallest entropy) bestSplit (The optimal line to split the attributes)
 def findBestSplitAux(initSplit, attrList, resultList):
     # This function tries to find the best split by checking:
@@ -92,7 +94,7 @@ def findBestSplitAux(initSplit, attrList, resultList):
         counterPosRight = 0;
         counterNegRight = 0;
         for i in range(0, attrList.__len__()):
-            if is_number(attrList[i]):
+            if isinstance(attrList[i], float):
                 if attrList[i] < tryLeftSplit:
                     if resultList[i] == 1:
                         counterPosLeft += 1
@@ -119,7 +121,7 @@ def findBestSplitAux(initSplit, attrList, resultList):
         counterPosRight = 0;
         counterNegRight = 0;
         for i in range(0, attrList.__len__()):
-            if is_number(attrList[i]):
+            if isinstance(attrList[i], float):
                 if attrList[i] < tryRightSplit:
                     if resultList[i] == 1:
                         counterPosLeft += 1
@@ -146,14 +148,6 @@ def findBestSplitAux(initSplit, attrList, resultList):
 
 #region helper function
 
-# Returns whether s is a float.
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
-    return False
 
 # Returns the float representation of s. If s is not a float, return s.
 def try_to_float(s):
@@ -165,15 +159,55 @@ def try_to_float(s):
 
 # Calculate the entropy. Input: number of positives and number of negatives, NOT probability.
 def entropy(pos, neg):
-    pos = float(pos)
-    neg = float(neg)
     if pos == 0 or neg == 0:
         return 0
     else:
-        return -(
-            neg / (neg + pos) * math.log(neg / (neg + pos), 2) + pos / (neg + pos) * math.log(pos / (neg + pos), 2))
+        pos = float(pos)
+        neg = float(neg)
+        sum = neg + pos
+        return - (neg/sum * math.log(neg/sum, 2) + pos/sum * math.log(pos/sum, 2))
 
 #endregion
 
-readTrainFile()
-findBestSplit()
+
+# the main function for building a decision tree
+# follow the algorithm in the slides
+def buildDecisionTree(data, default, height):
+    dataSize = len(data[0])-1
+    # if the data set is empty, return the default
+    if (dataSize == 0):
+        return default
+    # if all the data left have the same result, return it
+    elif (data[-1][1:] == dataSize * data[-1][1]):
+        return data[-1][1]
+    # if the tree height reaches the maximum, then return the majority of the result so far
+    elif (height == maxHeight):
+        return round(sum(data[-1][1:])/dataSize)
+    # else, compute the tree
+    else:
+        best_attr, best_split, min_ent = findBestSplit(data)
+        tree = [best_attr, best_split]
+        less = [[data[i][0]] for i in range(ATTR_SIZE)]
+        more = [[data[i][0]] for i in range(ATTR_SIZE)]
+        for i in range(1, dataSize+1):
+            if (data[best_attr][i] < best_split):
+                for j in range(ATTR_SIZE):
+                    less[j].append(data[j][i])
+            else:
+                for j in range(ATTR_SIZE):
+                    more[j].append(data[j][i])
+        print "Less size: " + str(len(less[0])) + "  More size: " + str(len(more[0]))
+        tree.append(buildDecisionTree(less, 1, height+1))
+        tree.append(buildDecisionTree(more, 1, height+1))
+        return tree
+
+
+
+
+
+maxHeight = 3
+thedata = readTrainFile()
+#print(data)
+#findBestSplit(thedata)
+print(buildDecisionTree(thedata, 1, 0))
+
